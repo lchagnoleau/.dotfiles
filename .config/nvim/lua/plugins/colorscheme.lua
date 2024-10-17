15bed29d8d45ed3be283e9e9672e443784c3cbb6
Loïc Chagnoleau <lchagnoleau@pm.me>
2024-10-17 21:08:39 +0200

    nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,22 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    require("tokyonight").setup({
-      style = "night",
-    })
-    vim.cmd([[colorscheme tokyonight]])
-  end,
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+      })
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+      require("monokai-pro").setup()
+    end,
+  }
 }
+
